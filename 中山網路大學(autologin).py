6df9f8711525d6f595b570d11ae3de98119e390c
yoyojoejoe <47710378+yoyojoejoe@users.noreply.github.com>
2019-11-23 23:59:48 +0800

    Add files via upload

---
+++ 中山網路大學(autologin).py
@@ -0,0 +1,15 @@
+import os
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+chromedriver = "E:\chromedriver\chromedriver.exe"		#取得chromedriver位置
+os.environ["webdriver.chrome.driver"] = chromedriver	#調用chromedriver
+driver = webdriver.Chrome(chromedriver)					#用selenium控制webdriver
+driver.get("https://cu.nsysu.edu.tw/")					#以google開啟中山網路大學
+elem_user = driver.find_element_by_name("username")
+elem_user.send_keys("B085090023")						#輸入username
+elem_pwd = driver.find_element_by_name("password")
+elem_pwd.send_keys("yoyojoejoe20049")					#輸入password
+elem_pwd.send_keys(Keys.RETURN)
